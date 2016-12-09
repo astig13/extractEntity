3b287150db5d710f6a5536d0b29acca7a777ef67
georgianaboita <georgiana.boita@yahoo.com>
2016-12-09 10:01:57 +0200

    fsfsfd

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,103 +1,172 @@
 'use strict'
 
-const firstOfEntityRole = function(message, entity, role) {
-  role = role || 'generic';
+const firstOfEntityRole = function (message, entity, role) {
+    role = role || 'generic';
 
-  const slots = message.slots
-  const entityValues = message.slots[entity]
-  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+    const slots = message.slots
+    const entityValues = message.slots[entity]
+    const valsForRole = entityValues ? entityValues.values_by_role[role] : null
 
-  return valsForRole ? valsForRole[0] : null
+    return valsForRole ? valsForRole[0] : null
 }
 
 exports.handle = function handle(client) {
-  // Create steps
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
+    // Create steps
+    const sayHello = client.createStep({
+        satisfied() {
+            return Boolean(client.getConversationState().helloSent)
+        },
 
-    prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
+        prompt() {
+            client.addResponse('welcome')
+            client.addResponse('provide/documentation', {
+                documentation_link: 'http://docs.init.ai',
+            })
+            client.addResponse('provide/instructions')
 
-      client.updateConversationState({
-        helloSent: true
-      })
+            client.updateConversationState({
+                helloSent: true
+            })
 
-      client.done()
-    }
-  })
-  
-  const collectCity = client.createStep({
-  satisfied() {
-	console.log('conversation state:', client.getConversationState().weatherCity) 
-    return Boolean(client.getConversationState().weatherCity)
-  },
-  
-  extractInfo() {
-    const city = firstOfEntityRole(client.getMessagePart(), 'number')
+            client.done()
+        }
+    })
 
-    if (city) {
-	  console.log('update conversation state:', city.value)
-      client.updateConversationState({
-        weatherCity: city,
-      })
+    const collectCity = client.createStep({
+        satisfied() {
+            console.log('conversation state:', client.getConversationState().weatherCity)
+            return Boolean(client.getConversationState().weatherCity)
+        },
 
-      console.log('User wants the weather in:', city.value)
-    }
-  },
+        extractInfo() {
+            const city = firstOfEntityRole(client.getMessagePart(), 'number')
 
-  prompt() {
-    // Need to prompt user for city    
-	//client.addResponse('provide_information/city')
-	client.addResponse('app:response:name:ask_information/number')
-    console.log('Need to ask user for city')
-    client.done()
-  },
-})
+            if (city) {
+                console.log('update conversation state:', city.value)
+                client.updateConversationState({
+                    weatherCity: city,
+                })
 
-const formalHello = client.createStep({
-  satisfied() {
-	//console.log('conversation state:', client.getConversationState().weatherCity) 
-      //return Boolean(client.getConversationState().weatherCity)
-      return false
-  },
+                console.log('User wants the weather in:', city.value)
+            }
+        },
 
-  prompt() {
-    // Need to prompt user for city    
-	//client.addResponse('provide_information/city')
-      client.addTextResponse('Hello user ' + city.value +'. How can I help you')
-    console.log('Need to ask user for city')
-    client.done()
-  },
-})
+        prompt() {
+            // Need to prompt user for city    
+            //client.addResponse('provide_information/city')
+            client.addResponse('app:response:name:ask_information/number')
+            console.log('Need to ask user for city')
+            client.done()
+        },
+    })
 
-  const untrained = client.createStep({
-    satisfied() {
-      return false
-    },
+    const handleCustomerRegistered = client.createStep({
+        satisfied() {
+            return false
+        },
+        prompt() {
+            client.addTextResponse("Register affirmative")
+            client.done()
+        }
+    })
 
-    prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
-    }
-  })
+    const handleCustomerNotRegistered = client.createStep({
+        satisfied() {
+            return false
+        },
+        prompt() {
+            client.addTextResponse("Registered declined")
+            client.done()
+        }
+    })
 
-  client.runFlow({
-    classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
-    streams: {
-      main: 'getInfoCity',
-      hi: [sayHello],
-	  getInfoCity: [collectCity, formalHello],
-    },
-  })
+    const formalHello = client.createStep({
+        satisfied() {
+            //console.log('conversation state:', client.getConversationState().weatherCity) 
+            //return Boolean(client.getConversationState().weatherCity)
+            return false
+        },
+
+        //next() {
+        //    const IsRegistered = false
+        //    if (IsRegistered === true) {
+        //        return 'customerRegistered'
+        //    } else if (IsRegistered === false) {
+        //        return 'customerNotRegistered'
+        //    }
+        //},
+
+        prompt() {
+            //client.addTextResponse('Hello user ' + client.getConversationState().weatherCity.value + '. How can I help you')
+            client.addTextResponse('Hello user ' + client.getConversationState().weatherCity.value + '. Do you want to be registered?')
+            //let baseClassification = client.getMessagePart().classification.base_type.value
+            //if (baseClassification === 'affirmative') {
+            //    console.log('User said YES')
+            //    return 'init.proceed' // `next` from this step will get called
+            //} else if (baseClassification === 'decline') {
+            //    console.log('User said NO')
+            //    return 'init.proceed' // `next` from this step will get called
+            //}
+            //client.expect(client.getStreamName(), ['affirmative', 'decline'])
+            client.done()
+        },
+    })
+
+    const checkRegistration = client.createStep({
+        satisfied() {
+            //console.log('conversation state:', client.getConversationState().weatherCity) 
+            //return Boolean(client.getConversationState().weatherCity)
+            return false
+        },
+
+        next() {
+            const IsRegistered = false
+            if (IsRegistered === true) {
+                return 'customerRegistered'
+            } else if (IsRegistered === false) {
+                return 'customerNotRegistered'
+            }
+        },
+
+        prompt() {
+            //client.addTextResponse('Hello user ' + client.getConversationState().weatherCity.value + '. How can I help you')
+            let baseClassification = client.getMessagePart().classification.base_type.value
+            if (baseClassification === 'affirmative') {
+                console.log('User said YES')
+                return 'init.proceed' // `next` from this step will get called
+            } else if (baseClassification === 'decline') {
+                console.log('User said NO')
+                return 'init.proceed' // `next` from this step will get called
+            }
+            client.expect(client.getStreamName(), ['affirmative', 'decline'])
+            client.done()
+        },
+    })
+
+    const untrained = client.createStep({
+        satisfied() {
+            return false
+        },
+
+        prompt() {
+            client.addResponse('apology/untrained')
+            client.done()
+        }
+    })
+
+    client.runFlow({
+        classifications: {
+            // map inbound message classifications to names of streams
+        },
+        autoResponses: {
+            // configure responses to be automatically sent as predicted by the machine learning model
+        },
+        streams: {
+            main: 'getInfoCity',
+            hi: [sayHello],
+            getInfoCity: [collectCity, formalHello, checkRegistration],
+            customerRegistered: [handleCustomerRegistered],
+            customerNotRegistered: [handleCustomerNotRegistered]
+        },
+    })
 }
